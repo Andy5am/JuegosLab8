be78b83850986a73122def12d9cd2881f0b4ee4f
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-05-21 07:05:53 -0600

    final

---
+++ Lab8/Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public AudioClip click;
+    private bool on = true;
+    public Text texto;
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(myRay,out hitInfo))
+        {
+            if (hitInfo.collider.gameObject.CompareTag("Objeto"))
+            {
+                canvas.transform.Find("Panel").gameObject.SetActive(true);
+                texto.text = hitInfo.collider.name;
+                if(hitInfo.collider.name == "Puerta")
+                {
+                    texto.text = hitInfo.collider.name + "\nPresione E para abrir";
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        Destroy(hitInfo.collider.gameObject);
+                    }
+                }
+            }else if (!hitInfo.collider.gameObject.CompareTag("Objeto"))
+            {
+                canvas.transform.Find("Panel").gameObject.SetActive(false);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (on)
+            {
+                GetComponent<AudioSource>().PlayOneShot(click);
+                on = !on;
+                GetComponent<Light>().intensity = 0;
+            }
+            else if (!on)
+            {
+                GetComponent<AudioSource>().PlayOneShot(click);
+                on = !on;
+                GetComponent<Light>().intensity = 3;
+            }
+        }
+    }
+}
---
+++ Lab8/Assets/light.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Luz"))
+        {
+            other.gameObject.GetComponent<Light>().color = Color.red;
+            other.gameObject.GetComponent<Light>().intensity = Mathf.Sin(Time.time)*5;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Luz"))
+        {
+            other.gameObject.GetComponent<Light>().intensity = 0;
+        }
+    }
+}
